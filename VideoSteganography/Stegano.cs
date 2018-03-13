bea51461937f2280dab1e00c9312ecac21bba2c6
Иван Костяшов <onlyno21@gmail.com>
2018-03-13 17:02:58 +0300

    Deleted binary color code, cause it's no need in it.

--- VideoSteganography/Stegano.cs
+++ VideoSteganography/Stegano.cs
@@ -11,7 +11,7 @@ namespace VideoSteganography
         public Bitmap Image { get; private set; }
         public Bitmap Watermark { get; private set; }
         public Bitmap EnscryptedImage { get; private set; }
-        private List<string> palette;
+        private List<int> palette;
         private readonly Color encryptionColour;
 
         public Stegano(string image, Color colour)
@@ -21,13 +21,6 @@ namespace VideoSteganography
             palette = GetPallete();
             EnscryptedImage = this.Image;
 
-        }
-        
-        public void Encrypt(Bitmap watermark)
-        {
-            this.Watermark = watermark;
-            if (GetPixelArray(Color.Black, Watermark).Length > GetPixelArray(Color.Blue, this.Image).Length) throw new IndexOutOfRangeException("ЦВЗ больше исходного изображения");
-            WriteEnscryptedImage();
         }
 
         #region Palette
@@ -47,7 +40,7 @@ namespace VideoSteganography
             return result;
         }
 
-        private List<string> GetPallete()
+        private List<int> GetPallete()
         {
             var palette = new List<string>();
             var pixels = GetPixelList(Image).Distinct();
@@ -57,78 +50,61 @@ namespace VideoSteganography
                 sortedPixels.Add(pixel);
             }
             sortedPixels.Sort();
-            foreach (var pixel in sortedPixels)
-            {
-                palette.Add(Convert.ToString(pixel, 2));
-            }
-            return palette;
+            return sortedPixels;
         }
 
         #endregion
+
+        public void Encrypt(Bitmap watermark)
+        {
+            this.Watermark = watermark;
+            if (GetPixelArray(Color.Black, Watermark).Length > GetPixelArray(Color.Blue, this.Image).Length) throw new IndexOutOfRangeException("ЦВЗ больше исходного изображения");
+            WriteEnscryptedImage();
+        }
+
+
 
         #region Encrypt
-
-
-
-
-        #endregion
-
-
+        
         private int[,] GetPixelArray(Color colour, Bitmap image)
         {
-            int[,] result = new int[image.Width,image.Height];
+            int[,] result = new int[image.Width, image.Height];
             for (int i = 0; i < image.Width; i++)
-            for (int j = 0; j < image.Height; j++)
-            {
-                result[i,j]= colour == Color.Black
-                        ?(int)image.GetPixel(i,j).GetBrightness()
-                        :colour == Color.Blue
-                        ? image.GetPixel(i, j).B
-                        : colour == Color.Green
-                        ? image.GetPixel(i, j).G
-                        : image.GetPixel(i, j).R;
-            }
+                for (int j = 0; j < image.Height; j++)
+                {
+                    result[i, j] = colour == Color.Black
+                            ? (int)image.GetPixel(i, j).GetBrightness()
+                            : colour == Color.Blue
+                            ? image.GetPixel(i, j).B
+                            : colour == Color.Green
+                            ? image.GetPixel(i, j).G
+                            : image.GetPixel(i, j).R;
+                }
             return result;
         }
-
        
-
-        private string[,] GetImageBytes(Bitmap image)
-        {
-            var pixels = GetPixelArray(encryptionColour,image);
-            string[,] result = new string[pixels.GetLength(0),pixels.GetLength(1)];
-            for (int i = 0; i < pixels.GetLength(0); i++)
-            for (int j = 0; j < pixels.GetLength(1); j++)
-            {
-                result[i, j] = Convert.ToString(pixels[i, j],2);
-            }
-            return result;
-        }
-        
-        private int[,] GetEncryptedImage()
-        {
-            var result = GetPixelArray(encryptionColour, Image);
-            var imageInBytes = GetImageBytes(Image);
-            var watermarkBytes = GetPixelArray(Color.Black, Watermark);
-            int i;
-            int j;
-            for (i = 0; i < watermarkBytes.GetLength(0); i++)
-            for (j = 0; j < watermarkBytes.GetLength(1); j++)
-            {
-                if (watermarkBytes[i, j] == 1) ChangeColour(ref imageInBytes[i,j]);
-                result[i, j] = Convert.ToInt32(imageInBytes[i, j], 2);
-            }
-            return result;
-        }
-
-        private void ChangeColour(ref string pixel)
+        private void ChangeColour(ref int pixel)
         {
             pixel = palette.IndexOf(pixel) == palette.Count - 1 ? palette[palette.IndexOf(pixel) - 1] : palette[palette.IndexOf(pixel) + 1];
         }
 
-        private Color GetNewColor(int i, int j,int value)
+        private int[,] GetEncryptedImage()
         {
-            if (encryptionColour==Color.Blue) return Color.FromArgb(Image.GetPixel(i,j).R,Image.GetPixel(i,j).G,value);
+            var result = GetPixelArray(encryptionColour, Image);
+            var watermarkBytes = GetPixelArray(Color.Black, Watermark);
+            int i;
+            int j;
+            for (i = 0; i < watermarkBytes.GetLength(0); i++)
+                for (j = 0; j < watermarkBytes.GetLength(1); j++)
+                {
+                    if (watermarkBytes[i, j] == 1) ChangeColour(ref result[i, j]);
+                }
+            return result;
+        }
+
+        private Color GetNewColor(int i, int j, int value)
+        {
+            if (encryptionColour == Color.Blue) return Color.FromArgb(Image.GetPixel(i, j).R, Image.GetPixel(i, j).G, value);
             else if (encryptionColour == Color.Green)
                 return Color.FromArgb(Image.GetPixel(i, j).R, value, Image.GetPixel(i, j).B);
             else return Color.FromArgb(value, Image.GetPixel(i, j).G, Image.GetPixel(i, j).B);
@@ -138,12 +114,16 @@ namespace VideoSteganography
         {
             var enscryptedBytes = GetEncryptedImage();
             for (int i = 0; i < EnscryptedImage.Width; i++)
-            for (int j = 0; j < EnscryptedImage.Height; j++)
-            {
-                Color pixel = EnscryptedImage.GetPixel(i, j);
-                EnscryptedImage.SetPixel(i,j,GetNewColor(i,j,enscryptedBytes[i,j]));
-            }
+                for (int j = 0; j < EnscryptedImage.Height; j++)
+                {
+                    Color pixel = EnscryptedImage.GetPixel(i, j);
+                    EnscryptedImage.SetPixel(i, j, GetNewColor(i, j, enscryptedBytes[i, j]));
+                }
         }
+        #endregion
+
+
+
 
         public void Decrypt(Bitmap encrypted)
         {
